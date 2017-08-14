4d60a0b64ca05c1bb01a347fa6eb8174fb3abb4f
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2017-08-14 03:06:10 +0200

    Remove unused fields from Question decoder

--- src/Data/Question.elm
+++ src/Data/Question.elm
@@ -1,12 +1,11 @@
 module Data.Question exposing (Question, decoder)
 
-import Json.Decode exposing (Decoder, map, map4, field, list, string)
+import Json.Decode exposing (Decoder, map, map3, field, list, string)
 import Util exposing (replaceHtmlEntities)
 
 
 type alias Question =
     { userAnswer : Maybe String
-    , category : String
     , question : String
     , correct : String
     , incorrect : List String
@@ -20,8 +19,7 @@ stringWithHtmlEntities =
 
 decoder : Decoder Question
 decoder =
-    map4 (Question Nothing)
-        (field "category" string)
+    map3 (Question Nothing)
         (field "question" stringWithHtmlEntities)
         (field "correct_answer" string)
         (field "incorrect_answers" (list stringWithHtmlEntities))
